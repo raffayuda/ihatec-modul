0266b66061c14f480d823a9209fc62d407731b51
raffayuda <rafffa0206@gmail.com>
2026-06-21 10:53:15 +0700

    feat: create mailable classes for module request submissions, processing updates, and approvals

--- app/Mail/ModuleRequestProcessedMail.php
+++ app/Mail/ModuleRequestProcessedMail.php
@@ -10,7 +10,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class ModuleRequestProcessedMail extends Mailable
+class ModuleRequestProcessedMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
